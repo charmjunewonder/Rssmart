0468456374e82317b133b79413a3ab568c7b5001
Eric <charmjunewonder@gmail.com>
2014-05-07 16:43:52 +0800

    change ui

---
+++ Rssmart/ECRecommender.h
@@ -0,0 +1,13 @@
+//
+//  ECRecommender.h
+//  Rssmart
+//
+//  Created by charmjunewonder on 5/7/14.
+//  Copyright (c) 2014 Eric Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECRecommender : NSObject
+
+@end
---
+++ Rssmart/ECRecommender.m
@@ -0,0 +1,13 @@
+//
+//  ECRecommender.m
+//  Rssmart
+//
+//  Created by charmjunewonder on 5/7/14.
+//  Copyright (c) 2014 Eric Chen. All rights reserved.
+//
+
+#import "ECRecommender.h"
+
+@implementation ECRecommender
+
+@end
